7d2844c9342d1e6d2bf7dd17a64e4e1908f332c2
ThomasZadikian <Thoma.zadikian@gmail.com>
2026-04-27 10:49:10 +0200

    feat : add init unit test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,21 @@
 import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
 import vuetify from 'vite-plugin-vuetify'
+import { defineConfig } from 'vitest/config'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({ autoImport: true })
-  ],
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'https://localhost:5001',
-        changeOrigin: true,
-        secure: false
-      }
+  plugins: [vue(),vuetify({autoImport: true})],
+  resolve: {alias: { '@': '/src'}},
+
+  test: {
+    globals: true, 
+    environment: 'jsdom',
+    setupFiles: ['./src/tests/setup.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text','lcov'],
+      include: ['src/**/*.{ts,vue}'], 
+      exclude: ['src/tests/**','src.main/ts'], 
+      thresholds: {lines:70},
     }
   }
 })
